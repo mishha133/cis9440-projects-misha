b6776b21187d7f12e325b9021301364b5a3da179
mishha133 <misha.asif@baruchmail.cuny.edu>
2026-04-30 14:11:05 -0400

    Refactor SQL query to combine location data sources

--- models/group_16_project/marts/shared/dim_shared_location.sql
+++ models/group_16_project/marts/shared/dim_shared_location.sql
@@ -1,25 +1,51 @@
-WITH locations AS (
-
+-- models/marts/shared/dim_shared_location.sql
+WITH locations_311 AS (
     SELECT DISTINCT
         borough,
-        zip_code
-
+        incident_zip AS zip_code,
+        street_name,
+        cross_street_1 AS cross_street_name,
+        cross_street_2 AS off_street_name,
+        latitude,
+        longitude
     FROM {{ ref('stg_nyc_311_vehicle_complaints') }}
-
     WHERE borough IS NOT NULL
-       OR zip_code IS NOT NULL
+),
 
+locations_crashes AS (
+    SELECT DISTINCT
+        borough,
+        zip_code,
+        on_street_name AS street_name,
+        cross_street_name,
+        off_street_name,
+        latitude,
+        longitude
+    FROM {{ ref('stg_nyc_vehicle_crashes') }}
+    WHERE borough IS NOT NULL
+),
+
+combined AS (
+    SELECT * FROM locations_311
+    UNION DISTINCT
+    SELECT * FROM locations_crashes
 ),
 
 final AS (
-
     SELECT
-        ROW_NUMBER() OVER (ORDER BY borough, zip_code) AS location_key,
+        {{ dbt_utils.generate_surrogate_key([
+            'borough',
+            'zip_code',
+            'street_name'
+        ]) }} AS location_key,
         borough,
-        zip_code
-
-    FROM locations
-
+        zip_code,
+        street_name,
+        cross_street_name,
+        off_street_name,
+        latitude,
+        longitude
+    FROM combined
 )
 
 SELECT * FROM final

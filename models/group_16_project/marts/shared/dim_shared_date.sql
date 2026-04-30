1732b90351b005c444349378452e66fb5269fa8d
mishha133 <misha.asif@baruchmail.cuny.edu>
2026-04-30 17:18:01 -0400

    Refactor date handling and add hour_of_day field

--- models/group_16_project/marts/shared/dim_shared_date.sql
+++ models/group_16_project/marts/shared/dim_shared_date.sql
@@ -1,14 +1,16 @@
 -- models/marts/shared/dim_shared_date.sql
 WITH dates AS (
     SELECT DISTINCT
-        DATE(created_date) AS full_date
+        CAST(created_date AS DATE) AS full_date,
+        FORMAT_TIME('%H:%M', CAST(created_date AS TIME)) AS hour_of_day
     FROM {{ ref('stg_nyc_311_vehicle_complaints') }}
     WHERE created_date IS NOT NULL
 
     UNION DISTINCT
 
     SELECT DISTINCT
-        DATE(crash_date) AS full_date
+        CAST(crash_date AS DATE) AS full_date,
+        FORMAT_TIME('%H:%M', PARSE_TIME('%H:%M', crash_time)) AS hour_of_day
     FROM {{ ref('stg_nyc_vehicle_crashes') }}
     WHERE crash_date IS NOT NULL
 ),
@@ -22,16 +24,18 @@ final AS (
         EXTRACT(DAY FROM full_date) AS day,
         FORMAT_DATE('%A', full_date) AS day_of_week,
         FORMAT_DATE('%B', full_date) AS month_name,
+        EXTRACT(DAYOFWEEK FROM full_date) IN (1, 7) AS is_weekend,
+        hour_of_day,
+        CASE
+            WHEN hour_of_day < '12:00' THEN 'AM'
+            ELSE 'PM'
+        END AS time_of_day,
         CASE
             WHEN EXTRACT(MONTH FROM full_date) IN (12, 1, 2) THEN 'Winter'
             WHEN EXTRACT(MONTH FROM full_date) IN (3, 4, 5) THEN 'Spring'
             WHEN EXTRACT(MONTH FROM full_date) IN (6, 7, 8) THEN 'Summer'
             ELSE 'Fall'
-        END AS season,
-        CASE
-            WHEN FORMAT_DATE('%A', full_date) IN ('Saturday', 'Sunday') THEN TRUE
-            ELSE FALSE
-        END AS is_weekend
+        END AS season
     FROM dates
 )
 
